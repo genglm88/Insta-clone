7ee5368f39eff6b98c613d465ca2aae8fd243cb6
genglm88 <108308458+genglm88@users.noreply.github.com>
2025-01-26 21:03:20 -0500

    params update2

--- src/app/(routes)/@modal/(.)post/[id]/page.tsx
+++ src/app/(routes)/@modal/(.)post/[id]/page.tsx
@@ -2,11 +2,14 @@ import Modal from "@/app/(routes)/components/Modal"
 import { singlePostData } from "@/app/(routes)/components/postingAction"
 import SinglePostContent from "@/app/(routes)/components/SinglePostContent"
 
-export default async function PostInModal({
-  params,
-}: {
-  params: { id: string }
-}) {
+// Define the type for `params`
+type PageProps = {
+  params: {
+    id: string;
+  };
+};
+
+export default async function PostInModal({params}: PageProps) {
   const { id } =  params
   const {
     existingPost,
