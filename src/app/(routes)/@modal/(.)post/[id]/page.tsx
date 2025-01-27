ff1b023e79e1634299407de8c4a9c5a82531cfb0
genglm88 <108308458+genglm88@users.noreply.github.com>
2025-01-27 07:30:24 -0500

    update params in post-id-page

--- src/app/(routes)/@modal/(.)post/[id]/page.tsx
+++ src/app/(routes)/@modal/(.)post/[id]/page.tsx
@@ -1,19 +1,20 @@
+import { use } from "react";
 import Modal from "@/app/(routes)/components/Modal"
 import { singlePostData } from "@/app/(routes)/components/postingAction"
 import SinglePostContent from "@/app/(routes)/components/SinglePostContent"
 
-// Define the type for `params`
-type IdPageProp = {
-  params:IdPageParam
-}
+// // Define the type for `params`
+// type IdPageProp = {
+//   params:IdPageParam
+// }
 
-type IdPageParam = {
-    id: string
-  }
+// type IdPageParam = {
+//     id: string
+//   }
 
 
-export default async function PostInModal(props: IdPageProp) {
-  const { id } = props.params
+export default async function PostInModal({params}:{params:Promise<{id:string}>}) {
+  const { id } = use(params)    
   const {
     existingPost,
     postAuthor,
