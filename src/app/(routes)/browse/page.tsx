d3e6d084af48c9eeabfd189776a8d30c90e4714f
genglm88 <108308458+genglm88@users.noreply.github.com>
2025-01-27 12:44:24 -0500

    fix user params-suspense

--- src/app/(routes)/browse/page.tsx
+++ src/app/(routes)/browse/page.tsx
@@ -1,11 +1,10 @@
-import { Suspense } from "react"
+// import { Suspense } from "react"
 import BrowsePosts from "../components/BrowsePost"
 import Spinner from "../components/Spinner"
 
 export default async function BrowsePage() {
   return (
-    <Suspense fallback={<Spinner />}>
-      <BrowsePosts />
-    </Suspense>
-  )
+ 
+      <BrowsePosts />)
+
 }
